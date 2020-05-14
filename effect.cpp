f799e0e48adace4f6b1411c08df9f6017a08c09c
sima-tosi <1916016@s.asojuku.ac.jp>
2020-05-14 15:08:12 +0900

    フェード

---
+++ effect.cpp
@@ -0,0 +1,68 @@
+#include "DxLib.h"
+#include "effect.h"
+#include "main.h"
+
+int fadeCnt;
+int fade;
+bool fadeIn;
+bool fadeOut;
+
+void FadeSysInit(void)
+{
+	fadeCnt = 0;
+	fade = FADE_NULL;
+	fadeIn = false;
+	fadeOut = false;
+}
+bool FadeIn(void)
+{
+	fadeCnt -= 4;
+	if (fadeCnt <= 0)
+	{
+		fadeIn = false;
+		fadeCnt = 0;
+		fade = FADE_NULL;
+		return true;
+	}
+	return false;
+}
+
+bool FadeOutB(void)
+{
+	fadeCnt += 4;
+
+	if (fadeCnt >= 255)
+	{
+		fadeCnt = 255;
+		fadeIn = true;
+		fadeOut = false;
+		return true;
+	}
+	return false;
+}
+bool FadeOutW(void)
+{
+	fadeCnt += 4;
+	if (fadeCnt >= 255)
+	{
+		fadeCnt = 255;
+		fadeIn = true;
+		fadeOut = false;
+		return true;
+	}
+	return false;
+}
+
+void FadeDraw(void)
+{
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, fadeCnt);
+	if (fade == FADE_WHITE)
+	{
+		DrawBox(0, 0, SCREEN_SIZE_X, SCREEN_SIZE_Y, 0xffffff, true);
+	}
+	 else if (fade == FADE_BLACK)
+	{
+		DrawBox(0, 0, SCREEN_SIZE_X, SCREEN_SIZE_Y, 0x000000, true);
+	}
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+}
