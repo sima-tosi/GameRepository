556ded2f43389af10b05950fd1a84221d11f0e02
sima-tosi <1916016@s.asojuku.ac.jp>
2020-05-15 16:54:52 +0900

    メイン

--- main.cpp
+++ main.cpp
@@ -6,8 +6,39 @@
 SCENE_ID sceneID;
 SCENE_ID sceneIDOld;
 
-int mouseX;
-int mouseY;
+int mouseX;			// マウスＸ座標
+int mouseY;			// マウスＹ座標
+int cnt;			// 背景用カウント
+int oCnt;			// 正解数
+int stage;		// ステージ数
+int life;			// 残機
+int time;			// 制限時間
+int A[3];			// 選択番号
+bool cutA[3];		// 切ってるか？
+bool check;			// 判定する？
+char quiz[3];		// 選択肢
+char Q;				// 問題文
+
+
+int gameFont;		// ライフ・時間フォント
+int quizFont;		// 問題文・選択肢フォント
+
+int oMusic;			// 正解音
+int xMusic;			// 不正解音
+
+int titleImage;		// タイトル
+int startWImage;	// スタートの板
+int ruleWImage;		// 遊び方説明の板
+int backWImage;		// 戻るの板
+int ruleImage;		// 遊び方説明
+int lifeImage;		// 残機の絵
+int timeImage;		// 時間の絵
+int toiImage;		// 問題
+int backImage[3][2];// 背景[ステージ][アニメーション]
+int woodImage[SIZE_END];	// 板[大・小]
+int reseulImage[2];	// リザルト[クリア・オーバー]
+int rankImage1[5];	// ランク[S・A・B・C・D]
+int rankImage2[3];	// ランク[上・中・下]
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -28,10 +59,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		if (fadeIn)FadeIn();
 
-		mouseX = -1;
-		mouseY = -1;
-
-		if (mouseDown[MOUSE_LEFT]) MousePos(&mouseX, &mouseY);
+		if (mouseDown[MOUSE_LEFT])
+		{
+			MousePos(&mouseX, &mouseY);
+		}
 
 		switch (sceneID)
 		{
@@ -46,11 +77,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 					if (FadeOutB())
 					{
 						sceneID = SCENE_GAME;
+						PlaySoundFile("music/game.mp3", DX_PLAYTYPE_LOOP);
+
 					}
-				}
-				if (fade == FADE_WHITE)
-				{
-					FadeOutW();
 				}
 			}
 			TitleScene();
@@ -63,6 +92,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 					if (FadeOutB())
 					{
 						sceneID = SCENE_RESULT;
+						PlaySoundFile("music/result.mp3", DX_PLAYTYPE_BACK);
 					}
 				}
 				if (fade == FADE_WHITE)
@@ -82,16 +112,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 						sceneID = SCENE_INIT;
 					}
 				}
-				if (fade == FADE_WHITE)
-				{
-					FadeOutW();
-				}
 			}
 			ResultScene();
+			break;
+		case SCENE_RULE:
+			RuleScene();
 			break;
 		default:
 			break;
 		}
+
+		MouseDraw(mouseX, mouseY);
 
 		FadeDraw();
 
@@ -112,43 +143,120 @@ bool SystemInit(void)	// 初期設定
 		return false;
 	}
 	SetDrawScreen(DX_SCREEN_BACK);
+	
+	// 音楽
+	oMusic = LoadSoundMem("music/O.mp3");
+	xMusic = LoadSoundMem("music/X.mp3");
+
+	// イラスト初期設定
+	{
+		titleImage = LoadGraph("image/タイトル.png");
+		startWImage = LoadGraph("image/START.png");
+		ruleWImage = LoadGraph("image/遊び方説明.png");
+		backWImage = LoadGraph("image/戻る.png");
+		ruleImage = LoadGraph("image/説明.png");
+		woodImage[0] = LoadGraph("image/問題.png");
+		woodImage[1] = LoadGraph("image/選択肢.png");
+		lifeImage = LoadGraph("image/ライフ.png");
+		timeImage = LoadGraph("image/時間.png");
+		toiImage = LoadGraph("image/問.png");
+		LoadDivGraph("image/1st.jpg", 2, 2, 1, SCREEN_SIZE_X, SCREEN_SIZE_Y, backImage[0]);
+		LoadDivGraph("image/2nd.jpg", 2, 2, 1, SCREEN_SIZE_X, SCREEN_SIZE_Y, backImage[1]);
+		LoadDivGraph("image/3rd.jpg", 2, 2, 1, SCREEN_SIZE_X, SCREEN_SIZE_Y, backImage[2]);
+	}
 
 	// 変数初期設定
 	sceneID = SCENE_INIT;
 	sceneIDOld = SCENE_MAX;
+	mouseX = -1;
+	mouseY = -1;
 	MouseSysInit();
 	FadeSysInit();
+
+	gameFont = CreateFontToHandle("GAME", 50, 3, -1);
+	quizFont = CreateFontToHandle("QUIZ", 70, 1, -1);
 
 	return true;
 }
 void SceneInit(void)	// 初期化
 {
+	oCnt = 0;
+	time = 1200;
+	life = 3;
+	check = false;
+	stage = 1;
+
 	sceneID = SCENE_TITLE;
+	PlaySoundFile("music/title.mp3", DX_PLAYTYPE_LOOP);
 }
 
 void TitleScene(void)
 {
-	if (mouseDown[MOUSE_LEFT])
+	if (SMALL_POS_X < mouseX && SMALL_POS_X + WOOD_SMALL_X > mouseX &&
+		START_POS_Y < mouseY && START_POS_Y + WOOD_SMALL_Y > mouseY &&
+		mouseDown[MOUSE_LEFT])
 	{
 		fadeOut = true;
 		fadeIn = false;
 		fade = FADE_BLACK;
 	}
-	if (mouseDown[MOUSE_RIGHT])
+	if (SMALL_POS_X < mouseX && SMALL_POS_X + WOOD_SMALL_X > mouseX &&
+		RULE_POS_Y < mouseY && RULE_POS_Y + WOOD_SMALL_Y > mouseY &&
+		mouseDown[MOUSE_LEFT])
 	{
-		fadeOut = true;
-		fade = FADE_WHITE;
+		sceneID = SCENE_RULE;
 	}
+
 	TitleDraw();
 }
 void TitleDraw(void)
 {
-	DrawBox(0, 0, 100, 100, 0xff0000,true);
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 200);
+	// 彪丸
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+	DrawGraph(100, 50, titleImage, true);
+	DrawGraph(SMALL_POS_X, START_POS_Y, startWImage, true);
+	DrawGraph(SMALL_POS_X, RULE_POS_Y, ruleWImage, true);
 }
 
 void GameScene(void)
 {
-	if (mouseDown[MOUSE_LEFT])
+	// if(問題が出たら)
+	if (CheckSoundMem(cutMusic) == 0)
+	{
+		if (check)
+		{
+			// 切っていい
+			PlaySoundMem(oMusic, DX_PLAYTYPE_BACK);
+
+			// きっちゃだめ
+			PlaySoundMem(xMusic, DX_PLAYTYPE_BACK);
+			life--;
+
+			check = false;
+		}
+	}
+	if (mouseDown[MOUSE_LEFT] && !check)
+	{
+		// 判定できるなら
+		check = true;
+	}
+	time--;
+	if (time <= 0)
+	{
+		PlaySoundMem(xMusic, DX_PLAYTYPE_BACK);
+		life--;
+		// 問題出てない
+	}
+
+	//if (oCnt % 4 == 0)
+	//{
+	//	fadeOut = true;
+	//	fade = FADE_WHITE;
+	//  stage++;
+	//}
+	if (life <= 0)
 	{
 		fadeOut = true;
 		fadeIn = false;
@@ -157,18 +265,42 @@ void GameScene(void)
 	if (mouseDown[MOUSE_RIGHT])
 	{
 		fadeOut = true;
-		fade = FADE_WHITE;
+		fadeIn = false;
+		fade = FADE_BLACK;
 	}
 	GameDraw();
 }
 void GameDraw(void)
 {
-	DrawBox(0, 0, 100, 100, 0x00ff00,true);
+	/*SetDrawBlendMode(DX_BLENDMODE_ALPHA, 50);*/
+
+	cnt++;
+	DrawGraph(0, 0, backImage[stage][cnt/5%2], true);
+
+	DrawGraph(75, 180, timeImage, true);
+	DrawFormatStringToHandle(200, 185, 0x000000,gameFont, "%d", time / 60);
+	DrawGraph(425, 180, lifeImage, true);
+	DrawFormatStringToHandle(550, 185, 0x000000,gameFont, "%d", life);
+
+	DrawGraph(50, 15, woodImage[BIG], true);
+	DrawFormatStringToHandle(50, 15, 0xffffff, quizFont, "%s", Q);
+	DrawGraph(SMALL_POS_X, A1_POS_Y, woodImage[SMALL], true);
+	DrawFormatStringToHandle(SMALL_POS_X, A1_POS_Y, 0xffffff, quizFont, "%s", quiz[0]);
+	DrawGraph(SMALL_POS_X, A2_POS_Y, woodImage[SMALL], true);
+	DrawFormatStringToHandle(SMALL_POS_X, A2_POS_Y, 0xffffff, quizFont, "%s", quiz[1]);
+	DrawGraph(SMALL_POS_X, A3_POS_Y, woodImage[SMALL], true);
+	DrawFormatStringToHandle(SMALL_POS_X, A3_POS_Y, 0xffffff, quizFont, "%s", quiz[2]);
+
+	//SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+	//DrawGraph(220, 233, toiImage, true);
 }
 
 void ResultScene(void)
 {
-	if (mouseDown[MOUSE_LEFT])
+	if (SMALL_POS_X < mouseX && SMALL_POS_X + WOOD_SMALL_X > mouseX &&
+		BACK_POS_Y < mouseY && BACK_POS_Y + WOOD_SMALL_Y > mouseY &&
+		mouseDown[MOUSE_LEFT])
 	{
 		fadeOut = true;
 		fadeIn = false;
@@ -183,5 +315,22 @@ void ResultScene(void)
 }
 void ResultDraw(void)
 {
-	DrawBox(0, 0, 100, 100, 0x0000ff,true);
+	DrawGraph(SMALL_POS_X, BACK_POS_Y, backWImage, true);
+}
+
+void RuleScene(void)
+{
+	if (SMALL_POS_X < mouseX && SMALL_POS_X + WOOD_SMALL_X > mouseX &&
+		BACK_POS_Y < mouseY && BACK_POS_Y + WOOD_SMALL_Y > mouseY &&
+		mouseDown[MOUSE_LEFT])
+	{
+		sceneID = SCENE_TITLE;
+	}
+
+	RuleDraw();
+}
+void RuleDraw(void)
+{
+	DrawGraph(0, 0, ruleImage, true);
+	DrawGraph(SMALL_POS_X, BACK_POS_Y, backWImage, true);
 }
--- main.h
+++ main.h
@@ -1,8 +1,20 @@
 #pragma once
 
 // 定義
-#define SCREEN_SIZE_X 700
-#define SCREEN_SIZE_Y 600
+#define SCREEN_SIZE_X	700
+#define SCREEN_SIZE_Y	600
+#define WOOD_SMALL_X	500
+#define WOOD_SMALL_Y	100
+
+#define SMALL_POS_X	100
+#define BIG_WOOD_X	 50
+#define START_POS_Y	325
+#define RULE_POS_Y	450
+#define BACK_POS_Y	475
+#define A1_POS_Y	255
+#define A2_POS_Y	370
+#define A3_POS_Y	485
+
 
 // シーン管理
 enum SCENE_ID
@@ -11,7 +23,19 @@ enum SCENE_ID
 	SCENE_TITLE,	// タイトル
 	SCENE_GAME,		// プレイ中
 	SCENE_RESULT,	// リザルト
+	SCENE_RULE,
 	SCENE_MAX		
+};
+
+enum SIZE {
+	BIG,
+	SMALL,
+	SIZE_END
+};
+
+struct XY {
+	int x;
+	int y;
 };
 
 // プロトタイプ宣言
@@ -24,3 +48,5 @@ void GameScene(void);
 void GameDraw(void);
 void ResultScene(void);
 void ResultDraw(void);
+void RuleScene(void);
+void RuleDraw(void);
